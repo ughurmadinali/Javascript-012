0b13b31db5b1e01fa7b646e445b273b9e27a953c
ughurmadinali <120271577+ughurmadinali@users.noreply.github.com>
2023-02-04 15:10:28 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+1
+//выведите в консоли текущий год
+let a = new Date()
+console.log(a.getFullYear());
+
+2
+//выведите в консоли месяцы актуального времени
+let b = new Date()
+console.log(b.getMonth());
+
+3
+//выведите в консоли часы актуального времени
+let v = new Date()
+console.log(v.getHours());
+
+4
+//выведите в консоли минуты актуального времени
+let q = new Date()
+console.log(q.getMinutes());
+
+5
+//выведите в консоли секунды актуального времени
+let d = new Date()
+console.log(d.getSeconds());
+
+6
+//выведите в консоли миллисекунды актуального времени
+let e = new Date()
+console.log(e.getMilliseconds());
+
+7
+//установите 2030 год и выведите в консоли 
+let yo = new Date()
+console.log(yo.setFullYear(2030));
+
+8
+//установите 12 часов и выведите в консоли 
+let j = new Date()
+console.log(j.setHours(12));
+
+9
+//установите 30 минут и выведите в консоли 
+let z = new Date()
+console.log(z.setMinutes(30));
+
+10
+//установите 2 месяц и выведите в консоли 
+let i = new Date()
+console.log(i.setMonth(2));
+
+11
+//установите 40 секунд и выведите в консоли 
+let k = new Date()
+console.log(k.setSeconds(40));
+
+12
+//установите 400 миллисекунд и выведите в консоли 
+let l = new Date()
+console.log(l.setMilliseconds(400));
+
+13
+function remainder(a, b) {
+    return a / b
+}
+console.log(remaineder(5, 2));
+
+14
+function findPerimeter(a, b) {
+    return (a + b)* 2
+}
+console.log(findPerimeter(6, 7));
+
+15
+function giveMeSomething() {
+    return
+}
+
+16
+
+
+17
+function myFunc(a) {
+    return a
+}
+console.log(myFunc(-55));
